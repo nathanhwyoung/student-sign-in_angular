ea8cb933435fde97e51bb824da11c8cec50e8b66
Nathan Young <nathanyoung@Nathans-MacBook-Pro.local>
2015-09-25 09:45:31 -0700

    add controller and factory

---
+++ controllers/StudentsController.js
@@ -0,0 +1,4 @@
+studentSignIn.controller('StudentsCtrl', function StudentsCtrl($scope, StudentsFactory) {
+  $scope.students = StudentsFactory.students;
+  $scope.StudentsFactory = StudentsFactory;
+});
